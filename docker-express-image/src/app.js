6881c6f93017fd8b75711d273429ecb477cc4164
Didier Page <page.didier@gmail.com>
2019-05-27 20:23:40 +0200

    Ajout des fichiers de base avec express

---
+++ docker-express-image/src/app.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const cors = require('cors');
+const createError = require('http-errors');
+const express = require('express');
+
+const chance = require('chance');
+
+const configFile = require('./config/config.json');
+
+const environment = process.env.NODE_ENV || 'dev';
+const config = configFile[environment];
+
+
+const { port } = config;
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+  res.json('Patate');
+});
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: true, message: error.message });
+});
+// test
+app.listen(port);
+module.exports = app;
